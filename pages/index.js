0b7aa2a659309490b8820bd6036c3b89d7eb4e75
22 <60903333+nini22P@users.noreply.github.com>
2023-04-05 09:14:15 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ export default function Home() {
     <>
       <Head>
         <title>Rinze</title>
-        <meta name="description" content="杜野凛世 Morino Rinze 04 70 " />
+        <meta name="description" content="杜野凛世 Morino Rinze 283 04 70 " />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
