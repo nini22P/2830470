1ea236a242e803cebdbd81ba3f51acb996b69b52
22 <60903333+nini22P@users.noreply.github.com>
2023-04-05 20:22:46 +0800

    Update json

--- pages/index.js
+++ pages/index.js
@@ -6,19 +6,7 @@ import Hover from 'react-3d-hover'
 const rinze_produce = require('/public/list/rinze_produce.json')
 const rinze_support = require('/public/list/rinze_support.json')
 
-const GalleryProduce = ({ list }) => list.map((card) => {
-  return (
-    <div className={styles.card} key={card.id}>
-      <Hover scale={1.025} speed={1000}>
-        <Image className={styles.image} src={card.src} alt={card.name} width={1136 / 4} height={640 / 4} title={card.name} />
-      </Hover>
-      <Hover scale={1.025} speed={1000}>
-        <Image className={styles.image} src={card.src_fes} alt={card.name + 'FES'} width={1136 / 4} height={640 / 4} title={card.name + ' +'} />
-      </Hover>
-    </div>)
-})
-
-const GallerySupport = ({ list }) => list.map((card) => {
+const Gallery = ({ list }) => list.map((card) => {
   return (
     <div className={styles.card} key={card.id}>
       <Hover scale={1.025} speed={1000}>
@@ -39,8 +27,8 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <gallery className={styles.gallery} >
-          <GalleryProduce list={rinze_produce} />
-          <GallerySupport list={rinze_support} />
+          <Gallery list={rinze_produce} />
+          <Gallery list={rinze_support} />
         </gallery>
       </main>
     </>
